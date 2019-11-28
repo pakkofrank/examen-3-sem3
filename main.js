b2e71f9fa4acbeb415189e89b6d2bd869ff335a7
pakkofrank <jpalma@gmail.com>
2019-11-28 15:46:10 -0600

    casi

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-agregar(nuevo){
-    if(nuevo.p){
+agregar(nuevo) {
+    if (nuevo.p) {
         var a = this._pri;
-        while(a.sig && a.p){
+        while (a.sig && a.p) {
             a = a.sig;
         }
+        if (a == this._pri) {
+            if (a.p) {
+                nuevo.sig = a.sig;
+                a.sig = nuevo;
+            } else {
+                nuevo.sig = a;
+                this._pri = nuevo;
+            }
+        }
+    } else if (a.sig) {
+        nuevo.sig = a.sig;
+        a.sig = nuevo;
 
-    }else{
+    } else {
         var a = this._pri;
-        while(a.sig){
-            a = a.sig; 
+        while (a.sig) {
+            a = a.sig;
         }
         a = nuevo;
     }
